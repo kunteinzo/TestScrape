4ea34a881674afca417eed5d7181b623af8d4561
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-07-17 18:50:54 +0630

    cls

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ def refresh_token():
         headers={
             "accept-encoding": "gzip",
             "content-type": "application/json",
-            "authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImE4ZGY2MmQzYTBhNDRlM2RmY2RjYWZjNmRhMTM4Mzc3NDU5ZjliMDEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbWFoYXItY2MyOTMiLCJhdWQiOiJtYWhhci1jYzI5MyIsImF1dGhfdGltZSI6MTc1MTY5NjQxNiwidXNlcl9pZCI6Ijg4Y2E2ZTRjLTVlMjctNGM3Ni1hNTJmLTVlYWQwZWVhOGU0NCIsInN1YiI6Ijg4Y2E2ZTRjLTVlMjctNGM3Ni1hNTJmLTVlYWQwZWVhOGU0NCIsImlhdCI6MTc1MjcyNzIxOCwiZXhwIjoxNzUyNzMwODE4LCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7fSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.A-NISGkr_tIo4beS_2Lu5o4lp5wIsFxqv1Mg7xcqxRh0vlNgdB1om0HXW2yEYn1Xs-6gWFQqTMmRInchANn8ND0OJ-jyHvMrsnEOUR5NI-_GzT80TeMVcn_soZHz17dz0p7QM5BsRZhHRGonGvNMPx3hqJi6VdMOZscX1F3YdU1LjZ53XODtOwNZR4qkj6d62G6gtzWQBIBE3Z3NsPQlEy5lvgdFNvBUlQVKoDKJ1Vnle3zZVKzaWu5YiO5ruCp_OQuvP4qWP84faA6UatIwJkNJUeR-wMGxfgsTd7BzsL1wscEkgUYmrWRVMcQjMof6O_vMaM2URPfeD8PZ-jOXSw"
+            "authorization": "Bearer tk"
         },
         data=json.dumps({
-            "refreshToken": "AMf-vBw4ehKn9frVEAvte_UkLSjIuntAgzC-b9wMJbEkDcyiXv5HdS-0aiJRPC3BfbEmTanxhPC_dKzwlSllYGEio08xqlUpcUpstzOfmRAWia6vp4kwim6m0XSqxaTtOGOca16Dbm54sKDjnxozhv9kw-RdSvJZVkYUDRmp-2jvymU_Aq7GgNFqigQ9Unv2lR3S7Lc8CJsy"
+            "refreshToken": ""
         }),
         isget=False
     ))['access_token']
