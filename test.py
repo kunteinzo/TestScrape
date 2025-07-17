5d6b1f5ab9d8f1eac1cf5536144d126f0b1d32d5
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-07-17 17:58:21 +0630

    clear test.py

--- test.py
+++ test.py
@@ -1,24 +1,3 @@
-import json, time
 
-with open("original.json") as f, open("newroot.json", "w") as f1:
-    data = json.loads(f.read())
-    fin = []
-    po = 1
-    total = len(data)
-    for dat in data:
-        new = dict(
-            id=dat['id'],
-            name=dat['name'],
-            is_series=dat['isSeries'],
-            is_subtitle=dat['isSubtitle'],
-            year=dat['prodYear'],
-            description=dat['synopsis'],
-            type=dat['type'],
-            rate=dat['vote'],
-            poster=dat['domainImage']+"/"+dat['poster'],
-            thumbnail=dat['domainImage']+"/"+dat['thumbnail']
-        )
-        fin.append(new)
-        print("Process: {:.2f}%".format((po/total)*100))
-        po += 1
-    f1.write(json.dumps(fin))
+
+print("Test some stuff")
