8cd181bc765635cd9b1ebfa5be2fdbd242107c5f
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-07-17 11:45:24 +0630

    testing mahar mobile api

--- main.py
+++ main.py
@@ -10,69 +10,62 @@ async def fetch_task(urls, mistext: bool = False):
                 if istext:
                     return await response.text()
                 return await response.json()
-        return await asyncio.gather(*[mf(url, session, mistext, dict(platform="android", version="1")) for url in urls])
+        return await asyncio.gather(*[mf(url, session, mistext, dict(platform="android-mobile", version="87")) for url in urls])
 
 
-async def content_fetch(url, text: bool|None = False):
+async def fetch(url, headers: dict|None = None, data: dict|None = None, isget: bool = True, text: bool|None = False):
     async with ClientSession() as ses:
-        async with ses.get(url, headers=dict(platform='android', version='1')) as rep:
-            if text == None:
-                return await rep.read()
-            if text:
-                return await rep.text()
-            return await rep.json()
+        if isget:
+            async with ses.get(url, headers=headers) as rep:
+                if text == None:
+                    return await rep.read()
+                if text:
+                    return await rep.text()
+                return await rep.json()
+        else:
+            async with ses.post(url, headers=headers, data=data) as rep:
+                if text == None:
+                    return await rep.read()
+                if text:
+                    return await rep.text()
+                return await rep.json()
 
 
-root = []
-r = asyncio.run(fetch_task([f"https://95tv.live/api/client/v1/content/display/movie/items?page={u}&length=10" for u in range(0,175)]))
-for rr in r:
-    root.extend(rr)
-fin = []
-for dat in root:
-        new = dict(
-            id=dat['id'],
-            name=dat['name'],
-            is_series=dat['isSeries'],
-            is_subtitle=dat['isSubtitle'],
-            year=dat['prodYear'],
-            description=dat['synopsis'],
-            type=dat['type'],
-            rate=dat['vote'],
-            poster=dat['domainImage']+"/"+dat['poster'],
-            thumbnail=dat['domainImage']+"/"+dat['thumbnail']
-        )
-        fin.append(new)
-        break
-
-r2 = asyncio.run(fetch_task([f"https://95tv.live/api/client/v1/content/movies/{mov['id']}" for mov in fin]))
-for rr in r2:
-    print(dict(
-        id=rr['id'],
-        duration=rr['duration'],
-        rate=rr['vote'],
-        name=rr['name'],
-        description=rr['synopsis'],
-        is_series=rr['isSeries'],
-        year=rr['prodYear'],
-        poster=rr['domainImage']+"/"+rr['poster'],
-        thumbnail=rr['domainImage']+"/"+rr['thumbnail']
+def refresh_token():
+    return asyncio.run(fetch(
+        "https://api.maharprod.com/profile/v1/RefreshToken",
+        headers={
+            "accept-encoding": "gzip",
+            "content-type": "application/json",
+            "authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImE4ZGY2MmQzYTBhNDRlM2RmY2RjYWZjNmRhMTM4Mzc3NDU5ZjliMDEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbWFoYXItY2MyOTMiLCJhdWQiOiJtYWhhci1jYzI5MyIsImF1dGhfdGltZSI6MTc1MTY5NjQxNiwidXNlcl9pZCI6Ijg4Y2E2ZTRjLTVlMjctNGM3Ni1hNTJmLTVlYWQwZWVhOGU0NCIsInN1YiI6Ijg4Y2E2ZTRjLTVlMjctNGM3Ni1hNTJmLTVlYWQwZWVhOGU0NCIsImlhdCI6MTc1MjcyNzIxOCwiZXhwIjoxNzUyNzMwODE4LCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7fSwic2lnbl9pbl9wcm92aWRlciI6ImN1c3RvbSJ9fQ.A-NISGkr_tIo4beS_2Lu5o4lp5wIsFxqv1Mg7xcqxRh0vlNgdB1om0HXW2yEYn1Xs-6gWFQqTMmRInchANn8ND0OJ-jyHvMrsnEOUR5NI-_GzT80TeMVcn_soZHz17dz0p7QM5BsRZhHRGonGvNMPx3hqJi6VdMOZscX1F3YdU1LjZ53XODtOwNZR4qkj6d62G6gtzWQBIBE3Z3NsPQlEy5lvgdFNvBUlQVKoDKJ1Vnle3zZVKzaWu5YiO5ruCp_OQuvP4qWP84faA6UatIwJkNJUeR-wMGxfgsTd7BzsL1wscEkgUYmrWRVMcQjMof6O_vMaM2URPfeD8PZ-jOXSw"
+        },
+        data=json.dumps({
+            "refreshToken": "AMf-vBw4ehKn9frVEAvte_UkLSjIuntAgzC-b9wMJbEkDcyiXv5HdS-0aiJRPC3BfbEmTanxhPC_dKzwlSllYGEio08xqlUpcUpstzOfmRAWia6vp4kwim6m0XSqxaTtOGOca16Dbm54sKDjnxozhv9kw-RdSvJZVkYUDRmp-2jvymU_Aq7GgNFqigQ9Unv2lR3S7Lc8CJsy"
+        }),
+        isget=False
     ))
-    break
 
-r3 = asyncio.run(fetch_task([f"https://95tv.live/api/client/v1/content/movies/{u['id']}/link" for u in r2]))
-test1 = {}
-for rr in r3:
-    test1 = dict(
-        name=rr['name'],
-        thumbnail=rr['domainImage']+"/"+rr['thumbnail'],
-        src=rr['domainVideo']+"/"+rr['link']
+def movie_home():
+    return json.loads(asyncio.run(
+        fetch(
+            "https://api.maharprod.com/display/v1/moviebuilder?pageNumber=1",
+            headers={
+                "accept": "application/json",
+                "content-type": "application/json",
+                "authorization": f"Bearer {refresh_token()['access_token']}"
+            },
+            text=True
+        )
+    ))
+
+def mov_detail():
+    return asyncio.run(
+        fetch(
+            "https://api.maharprod.com/content/v1/MovieDetail/165b9620-9db9-48b9-a624-f5ad5d070d73",
+            headers={
+                "authorization": f"Bearer {refresh_token()['access_token']}"
+            }
+        )
     )
-r4 = asyncio.run(content_fetch(test1['src'].replace('index.m3u8', '720/index.m3u8'), True))
 
-with open("test.m3.u8", "w") as f:
-    f.write(r4)
-
-#content = asyncio.run(mov_content_fetch("https://95tv.live/api/client/v1/content/movies/Mxbj34UHg"))
-#link = asyncio.run(mov_content_fetch("https://95tv.live/api/client/v1/content/movies/Mxbj34UHg/link"))
-#print(asyncio.run(mov_content_fetch("https://vod.95tv.live/public/uploads/2025/07/14/7b9dc785-c593-4671-9e92-bfa03df8fe14/Dcyge48Ng/index.m3u8?signKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZW50SUQiOiJEY3lnZTQ4TmcifQ.jbFNpH5B2LzSfVLNMcsAdutfbs0dHiueZyFEjhVygpo", True)))
-
+print(mov_detail())
