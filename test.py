27c9f19b8e950244684c248765b35da789b5f64c
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-07-18 15:54:39 +0630

    test db

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, select
+from sqlalchemy.orm import sessionmaker
 
-he = None
-hr = he or {'msg': 'hi'}
+engine = create_engine("mysql+pymysql://test:asdfgh@192.168.100.49/test")
+meta = MetaData()
 
-print(hr)
+user = Table(
+    "user", meta,
+    Column("uid", Integer, primary_key=True, autoincrement=True),
+    Column("name", String(100))
+)
+meta.create_all(engine)
+
+Session = sessionmaker(engine)
+
+with Session() as ss:
+    name = input("Name: ")
+    print(ss.query(user).filter(user.c.name == name).all())
