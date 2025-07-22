ec77ad523757b27d6e03b37a9e433a8db3b7d19a
Khun Thein Htet Oo <theinhtetoo473@gmail.com>
2025-07-22 16:19:15 +0630

    testing step by step

--- test.py
+++ test.py
@@ -6,4 +6,22 @@ api = Api()
 tk = run(s_fetch('post', api.url_refresh, token='tk', headers={'content-type': 'application/json'},
                  data=json.dumps(dict(refreshToken=api.refresh_token))))['access_token']
 
-print(run(s_fetch('get', api.url_movie_home(1), 'text', tk)))
+# Movie Home
+mov_home = json.loads(run(s_fetch('get', api.url_movie_home(1), 'text', tk)))['value']
+
+playlistId = mov_home[0]['playlistId']
+
+mov_from_playlist = run(s_fetch('get', api.url_playlist(playlistId, 1), token=tk))['value']
+
+movieId = mov_from_playlist[0]['id']
+
+movieDetail = run(s_fetch('get', api.url_movie_detail(movieId), token=tk))['value']
+
+mov_contentId = movieDetail['contentId']
+
+movieStream = run(s_fetch('get', api.url_movie_stream(mov_contentId), token=tk))
+
+movieDownloadLink = run(s_fetch('get', api.url_movie_download(mov_contentId, 'hd'), token=tk))
+
+# Movie Genres
+# print(run(s_fetch('get', api.url_movie_genres, token=tk)))
